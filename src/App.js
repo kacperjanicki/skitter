171a39f7cf3b360448de2f4a4bfb801b2ed0974a
kacperjanicki <kacperekjanicki9@gmail.com>
2022-06-17 14:02:57 +0200

    sending context works propertly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import "./components/loginpage.css";
 import LoginPage from "./components/LoginPage";
+import React, { useContext, useRef, useState, useEffect } from "react";
+import { auth } from "./firebase";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export const UserProvider = React.createContext();
 
 function App() {
-    return <LoginPage />;
+    const [currentUser, setcurrentUser] = useState();
+
+    const signup = (email, password) => {
+        return auth.createUserWithEmailAndPassword(email, password);
+    };
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setcurrentUser(user);
+        });
+        return unsubscribe;
+    }, []);
+
+    const value = {
+        currentUser,
+        signup,
+    };
+
+    return (
+        <UserProvider.Provider value={value}>
+            <Router>
+                <Routes>
+                    <Route path="/" exact element={<LoginPage />}></Route>
+                </Routes>
+            </Router>
+        </UserProvider.Provider>
+    );
 }
 
 export default App;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,16 @@
-import React, { useRef, useState } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
+import { auth } from "../firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useAuth } from "../context/authcontext";
-import { AuthProvider } from "../context/authcontext";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import "./loginpage.css";
-import { auth } from "../firebase";
-
-function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password);
-}
+import { UserProvider } from "../App";
 
 const LoginPage = () => {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
+    const { signup } = useContext(UserProvider);
+    console.log({ signup });
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
 
@@ -38,31 +35,30 @@ const LoginPage = () => {
             <div className="main">
                 <span id="title">Skitter</span>
                 <div className="login">
-                    <AuthProvider>
-                        <Card>
-                            <Card.Body>
-                                <h2>Sign up</h2>
-                                {error && <Alert variant="danger">{error}</Alert>}
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group id="email">
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control type="email" ref={emailRef} required></Form.Control>
-                                    </Form.Group>
-                                    <Form.Group id="password">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" ref={passwordRef} required></Form.Control>
-                                    </Form.Group>
-                                    <Form.Group id="password-confirm">
-                                        <Form.Label>Confirm Password</Form.Label>
-                                        <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
-                                    </Form.Group>
-                                    <Button className="w-100" type="submit" disabled={loading}>
-                                        Sign up
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </AuthProvider>
+                    <Card>
+                        <Card.Body>
+                            <h2>Sign up</h2>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group id="email">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" ref={emailRef} required></Form.Control>
+                                </Form.Group>
+                                <Form.Group id="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                                </Form.Group>
+                                <Form.Group id="password-confirm">
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
+                                </Form.Group>
+                                <Button className="w-100" type="submit" disabled={loading}>
+                                    Sign up
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+
                     <div className="w-100 text-center mt-2">Already have an Account? Log in</div>
                 </div>
             </div>
